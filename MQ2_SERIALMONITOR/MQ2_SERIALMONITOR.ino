ab8245377e97145f409bfa920064e175ca27f2a0
Harsh Kumar <125388784+harshkzz@users.noreply.github.com>
2023-04-01 13:35:11 +0530

    Add files via upload

---
+++ MQ2_SERIALMONITOR/MQ2_SERIALMONITOR.ino
@@ -0,0 +1,24 @@
+#include<MQ2.h>
+
+#define PIN A0
+
+MQ2 mq2(PIN);
+
+void setup() {
+  Serial.begin(9600);
+  mq2.begin();
+
+}
+
+void loop() {
+  int lpg = mq2.readLPG();
+  int co = mq2.readCO();
+  int smo = mq2.readSmoke();
+  Serial.print("Smoke: ");
+  Serial.print(smo);
+  Serial.print("  LPG: ");
+  Serial.print(lpg);
+  Serial.print("  CO2: ");
+  Serial.println(co);
+  delay(2000);
+}
